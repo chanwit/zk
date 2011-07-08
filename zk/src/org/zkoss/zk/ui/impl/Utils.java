5b91972eb7b2762e73421d732772f2c25dc78508
tomyeh <tomyeh@zkoss.org>
2011-07-08 09:35:09 +0000

    2947727: The instantiation of Composer & Server Push customizable http://www.zkoss.org/services/zk/tracker/?F2947727
    git-svn-id: https://svn.potix.com/svn/zk1/branches/5.1@17137 dd50bd9b-9913-0410-b9ba-e07a3075be86

--- zk/src/org/zkoss/zk/ui/impl/Utils.java
+++ zk/src/org/zkoss/zk/ui/impl/Utils.java
@@ -12,7 +12,12 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zk.ui.impl;
 
+import org.zkoss.lang.Classes;
+
 import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.util.Composer;
+import org.zkoss.zk.ui.sys.WebAppCtrl;
 
 /**
  * Utilities for implementation.
@@ -30,5 +35,31 @@ public class Utils {
 	boolean markClientInfoPerDesktop(Desktop desktop, String key) {
 		return !(desktop instanceof DesktopImpl) //always gen if unknown
 		|| ((DesktopImpl)desktop).markClientInfoPerDesktop(key);
+	}
+
+	/** Instantiates a composer of the given object.
+	 * This method will invoke {@link org.zkoss.zk.ui.sys.UiFactory#newComposer}
+	 * to instantiate the composer if page is not null.
+	 * @param page the page that the composer will be created for.
+	 * Ignored if null.
+	 * @param o the composer instance, the class of the composer to instantiate,
+	 * or the name of the class of the composer.
+	 * If <code>o</code> is an instance of {@link Composer}, it is returned
+	 * directly.
+	 */
+	public static Composer newComposer(Page page, Object o)
+	throws Exception {
+		Class cls;
+		if (o instanceof String) {
+			cls = page != null ? page.resolveClass((String)o):
+				Classes.forNameByThread(((String)o).trim());
+		} else if (o instanceof Class) {
+			cls = (Class)o;
+		} else
+			return (Composer)o;
+
+		if (page != null) 
+			return ((WebAppCtrl)page.getDesktop().getWebApp()).getUiFactory().newComposer(cls, page);
+		return (Composer)cls.newInstance();
 	}
 }
